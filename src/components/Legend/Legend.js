edbcbe71d458e7c8cbb4ae95738567c822b97e70
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-03-12 10:10:25 -0500

    Fixed issue with Legend

--- src/components/Legend/Legend.js
+++ src/components/Legend/Legend.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Legend = () => {
     return (
         <section className="fs-4">
-            <p className="p-2"><span><i className="fas fa-solid fa-play"></i> Start Node</span><span><i className="fas fa-solid fa-flag-checkered"></i> End Node</span><span><span className="empty"></span> Empty Node</span><span><span className="blocker"></span> Blocker Node</span><span><span className="explored"></span> Explored Node</span><span><span className="path"></span> Shortest Path</span></p>
+            <p className="p-2"><span><span className='start'></span> Start Node</span><span><span className='end'></span> End Node</span><span><span className="empty"></span> Empty Node</span><span><span className="blocker"></span> Blocker Node</span><span><span className="explored"></span> Explored Node</span><span><span className="path"></span> Shortest Path</span></p>
             <p className="center">Choose an algorithm to visualize!</p>
         </section>
     );
