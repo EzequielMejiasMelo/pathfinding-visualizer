01f30ca1b370a30606b9a2333da3f872251caad1
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-03-12 11:26:54 -0500

    Update node to blocker node

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,20 +4,34 @@ import Node from "./Node";
 class Board extends Component {
   constructor(props) {
     super(props);
-    this.state = { grid: [], start: [14, 17], end: [14, 47] };
+    this.state = {
+      grid: [],
+      start: [14, 17],
+      end: [14, 47],
+      mouseClick: false,
+    };
   }
 
   componentDidMount() {
-    this.clearBoard();
+    this.initialBoard();
   }
 
-  clearBoard() {
+  initialBoard() {
     let grid = [];
 
     for (let row = 0; row < 30; row++) {
       const currRow = [];
       for (let col = 0; col < 66; col++) {
-        const node = {row: row, column: col, rowCount: 30, columnCount: 66, type: 0, neighbors: [], distance: null, previous: null};
+        const node = {
+          row: row,
+          column: col,
+          rowCount: 30,
+          columnCount: 66,
+          type: 0,
+          neighbors: [],
+          distance: null,
+          previous: null,
+        };
         if (row === 14 && col === 17) {
           node.type = 3;
           currRow.push(node);
@@ -35,14 +49,45 @@ class Board extends Component {
     this.setState({ grid });
   }
 
-  render() {
-    const { grid } = this.state;
+  updateGridToBlocker(grid, row, col) {
+    const newGrid = grid.slice();
+    const oldNode = newGrid[row][col];
+    const newNode = { ...oldNode, type: 2 };
+    newGrid[row][col] = newNode;
+    return newGrid;
+  }
 
+  mouseDown = (row, col) => {
+    const newGrid = this.updateGridToBlocker(this.state.grid, row, col);
+    this.setState({ grid: newGrid, mouseClick: true });
+  }
+
+  mouseEnter = (row, col) => {
+    if (!this.state.mouseClick) return;
+    const newGrid = this.updateGridToBlocker(this.state.grid, row, col);
+    this.setState({ grid: newGrid });
+  }
+
+  mouseUp = () => {
+    this.setState({ mouseClick: false });
+  }
+
+  render() {
     return (
       <section className="grid-container">
-        {grid.map((row) => {
+        {this.state.grid.map((row) => {
           return row.map((node) => {
-            return <Node key={[node.row, node.column]} row={node.row} column={node.column} type={node.type} />
+            return (
+              <Node
+                key={[node.row, node.column]}
+                row={node.row}
+                column={node.column}
+                type={node.type}
+                MouseDown={this.mouseDown}
+                MouseEnter={this.mouseEnter}
+                MouseUp={this.mouseUp}
+              />
+            );
           });
         })}
       </section>
--- src/components/Board/Node.js
+++ src/components/Board/Node.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // 0 - unvisited node
 // 1 - visited node
@@ -8,46 +8,43 @@ import React, { Component } from 'react';
 // 5 - shortest path node
 
 class Node extends Component {
-    constructor(props) {
-        super(props);
-        this.row = props.row;
-        this.column = props.column;
-        this.state = {type: props.type};
-    };
-    
-    getPosition() {
-        return [this.row, this.column];
-    };
+  constructor(props) {
+    super(props);
+    this.row = props.row;
+    this.column = props.column;
+    this.type = props.type;
+  }
 
-    getType() {
-        return this.state.type;
-    };
+  getClass() {
+    switch (this.type) {
+      case 0:
+        return "empty";
+      case 1:
+        return "explored";
+      case 2:
+        return "blocker";
+      case 3:
+        return "start";
+      case 4:
+        return "end";
+      case 5:
+        return "path";
+      default:
+        break;
+    }
+  }
 
-    getClass() {
-        const { type } = this.state;
-        switch (type) {
-            case 0:
-                return 'empty';
-            case 1:
-                return 'explored';
-            case 2:
-                return 'blocker';
-            case 3:
-                return 'start';
-            case 4:
-                return 'end';
-            case 5:
-                return 'path';
-            default:
-                break;
-        };
-    };
-    
-    render() { 
-        return ( 
-            <div className={this.getClass()} id={[this.row, this.column]}></div>
-        );
-    };
-};
- 
+  render() {
+    return (
+      <div
+        className={this.getClass()}
+        id={[this.row, this.column]}
+        onMouseDown={() => this.props.MouseDown(this.row, this.column)}
+        onMouseEnter={() => this.props.MouseEnter(this.row, this.column)}
+        onMouseUp={() => this.props.MouseUp()}
+      ></div>
+    );
+  }
+}
+
 export default Node;
